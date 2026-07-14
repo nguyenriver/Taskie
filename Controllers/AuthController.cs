46f4d3babea023d93456abb4261938c9e19101f7
nguyenriver <nguyenriver27@gmail.com>
2026-07-14 07:42:49 +0700

    fix: Align login payload property casing of id to userID to resolve dashboard filters

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -66,7 +66,7 @@ namespace TaskieWNC.Controllers
                 token = token,
                 user = new
                 {
-                    id = user.UserID,
+                    userID = user.UserID,
                     email = user.Email,
                     fullName = user.FullName,
                     role = user.Role
